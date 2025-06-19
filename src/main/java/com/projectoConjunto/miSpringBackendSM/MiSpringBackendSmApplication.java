21837f2a2ff58c052e04138992d306672239f121
martinsnovo_tuf <martinwirtzfp@gmail.com>
2025-06-19 20:56:34 +0200

    proyecto creado

---
+++ src/main/java/com/projectoConjunto/miSpringBackendSM/MiSpringBackendSmApplication.java
@@ -0,0 +1,13 @@
+package com.projectoConjunto.miSpringBackendSM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiSpringBackendSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiSpringBackendSmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectoConjunto/miSpringBackendSM/MiSpringBackendSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectoConjunto.miSpringBackendSM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiSpringBackendSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
